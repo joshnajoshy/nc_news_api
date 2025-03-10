fc5f3de749835a5c722254a5f8fe6062a2401ab5
joshnajoshy <joshnajoshy@yahoo.co.uk>
2025-03-10 12:28:30 +0000

    save changes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const {getAllEndpoints, getAllTopics} = require('./controllers/api.controllers')
+const handleServerErrors = require('./controllers/errors.controllers')
 
 app.get("/api", getAllEndpoints);
 
 app.get('/api/topics', getAllTopics)
+
+app.use(handleServerErrors)
 
 module.exports = app;
